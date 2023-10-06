05eec3af20b541eabe7461d30d11bac2f373ddb1
pado35 <padoru1825@gmail.com>
2023-10-06 17:31:00 +0800

    k

---
+++ homework/01.js
@@ -0,0 +1,31 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+    console.log(ctx.request.url.pathname)
+    ctx.response.body = "Hello World!"
+    if(ctx.request.url.pathname=="/nqu/")
+        ctx.response.body = `
+        <html>
+            <body>
+                <a href="https://www.nqu.edu.tw/">nqu</a>
+            </body>
+        </html>`
+    else if(ctx.request.url.pathname=="/nqu/csie/")
+        ctx.response.body = `
+        <html>
+            <body>
+                <a href="https://csie.nqu.edu.tw/">csie_nqu</a>
+            </body>
+        </html>`
+    else if(ctx.request.url.pathname=="/to/nqu/")
+        ctx.response.redirect("https://csie.nqu.edu.tw/")
+    else if(ctx.request.url.pathname=="/to/nqu/csie/")
+        ctx.response.redirect("https://csie.nqu.edu.tw/")
+});
+
+console.log('start at : http://127.0.0.1:8000')
+await app.listen({ port: 8000 });
+
+
